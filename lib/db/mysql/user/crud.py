1d0d657b91ff76912a776c646a37097aef19a9d1
vladislav.lundyshev <vlunku@gmail.com>
2024-07-28 03:58:57 +0300

    Added crud for get_user

--- lib/db/mysql/user/crud.py
+++ lib/db/mysql/user/crud.py
@@ -7,3 +7,15 @@ async def create_user(user_info: ty.Dict[str, ty.Any]) -> None:
     with dbInstance().session() as session:
         session.execute(insert(user_table).values(**user_info))
         session.commit()
+
+async def get_user(user_login: str) -> ty.Dict[str, ty.Any]:
+    with dbInstance().session() as session:
+        user: ty.NamedTuple[str, ty.Any] = session.execute(
+            select(user_table).
+            where(
+                user_table.c.user_login==user_login
+            )
+        ).first()
+        session.commit()
+
+    return user
