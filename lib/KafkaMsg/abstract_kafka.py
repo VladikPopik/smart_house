8c717b1d4d98646070182ca1b2565d4d26b552e9
VLund <vlunku@gmail.com>
2024-10-08 15:36:16 +0000

    Base kafka generic classes

--- lib/KafkaMsg/abstract_kafka.py
+++ lib/KafkaMsg/abstract_kafka.py
@@ -1,11 +1,14 @@
 import typing as ty
 
+from collections.abc import AsyncGenerator
 from abc import ABC, abstractmethod
 
 
-class AbstractConsumer[T](ABC):
+class AbstractConsumer[T, R](ABC):
+    """Absctract Factory for Kafka Consumers."""
+
     @abstractmethod
-    def recieve(self, topic: str) -> dict[str, ty.Any]:
+    def recieve(self, topic: str) -> dict[str, R]:
         """Recieve message via kafka."""
         ...
 
@@ -15,11 +18,33 @@ class AbstractConsumer[T](ABC):
         ...
 
     @abstractmethod
-    def unsubscribe() -> None:
-        """Unsubscribe from all topics."""
+    def _read_data(self, msg: T) -> R:
+        """Cast data for receive method."""
         ...
 
     @abstractmethod
-    def _read_data(self, msg: T) -> T:
-        """Cast data for receive method."""
+    async def get_consumer(self) -> AsyncGenerator[ty.Self, None, None]:
+        """Context manager to get consumer."""
+        ...
+
+
+class AbstractProducer[T, R](ABC):
+    @abstractmethod
+    def send(self, topic: tuple[str, ...], value: T, key: str) -> None:
+        """Method to send message via kafka."""
+        ...
+
+    @abstractmethod
+    def close(self, timeout: int) -> bool:
+        """Close producer."""
+        ...
+
+    @abstractmethod
+    def _cast_data(self, data: T) -> R:
+        """Cast data into proper type."""
+        ...
+
+    @abstractmethod
+    async def get_producer(self) -> AsyncGenerator[ty.Self, None, None]:
+        """Context manager to get consumer."""
         ...
---
+++ lib/KafkaMsg/producers.py
@@ -0,0 +1,64 @@
+import typing as ty
+
+import json
+
+from .abstract_kafka import AbstractProducer
+from kafka import KafkaProducer
+
+from collections.abc import AsyncGenerator
+
+class BaseProducer[T, R](AbstractProducer[T, R]):
+    def __init__(self, **configs: dict[str, ty.Any]) -> None:
+        self._producer = KafkaProducer(**configs)
+
+    def send(self, topic: str, value: T, key: str) -> None:
+        """Method to send message via kafka."""
+        try:
+            value = self._convert_data(value)
+            self._producer.send(topic, value, key)
+        except Exception as e:
+            raise e
+
+    def close(self, timeout: int) -> bool:
+        """Close producer."""
+        self._producer.close(timeout)
+
+    def _cast_data(self, data: T) -> R:
+        return data
+
+    async def get_producer(self) -> AsyncGenerator[ty.Self, None, None]:
+        """Context manager to get consumer."""
+        producer = self._producer
+        try:
+            yield producer if producer.bootstrap_connected() else None
+        except Exception as e:
+            raise e
+
+json_type_alias: ty.TypeAlias = dict[str, ty.Any] | list[ty.Any]
+json_return_type_alias: ty.TypeAlias = dict[str, ty.Any]
+
+class JSONProducer(
+    BaseProducer[
+        json_type_alias,
+        json_return_type_alias
+    ]
+):
+    @ty.override
+    def _cast_data(self, data: json_type_alias) -> json_return_type_alias:
+        try:
+            return json.dump(data)
+        except json.JSONDecodeError as e:
+            raise e
+        
+
+class StrProducer(
+    BaseProducer[
+        str, bytes
+    ]
+):
+    @ty.override
+    def _cast_data(self, data: str) -> bytes:
+        try:
+            return data.encode()
+        except Exception as e:
+            raise e
