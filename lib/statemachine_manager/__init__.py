e2a92aa1bbd4dd7cea724a39aa135e1bbd36bb9d
vladislav.lundyshev <vlunku@gmail.com>
2024-11-07 13:43:44 +0300

    initial statemanager

---
+++ lib/statemachine_manager/__init__.py
@@ -0,0 +1,5 @@
+""" \
+This is a package that controls orchestration. \
+
+We have StateManager that controls flow of micro-services in the system.
+"""  # noqa: D205, D210
---
+++ lib/statemachine_manager/manager_stm.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+from statemachine.contrib.diagram import DotGraphMachine
+from statemachine import StateMachine, State
+from statemachine.states import States
+
+img_path = "/data/statemanager.png"
+
+class Status(int, Enum):
+    planned = 0
+    pending = 1
+    completed = 2
+    error = -1
+
+
+class StateManager(StateMachine):
+    """State machine that controls behaviour of micro-services."""
+
+    state_ = States.from_enum(Status, initial=Status.planned, final=Status.error)
+
+    move = state_.planned.to(state_.pending)
+    complete = state_.pending.to(state_.completed)
+    finish = state_.completed.to(state_.error)
+
+    def on_enter_error(self) -> None:
+        print(f"error occured")
+
+
+sm = StateManager()
+graph = DotGraphMachine(sm)
+graph().write("statemanager.jpg", format='jpg')
